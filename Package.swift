eb4d1e4834ac54a85c3ac37bc1f6ab47b8d96ea2
GabrielSilveiraa <gabi.msilveira@gmail.com>
2020-12-10 15:07:35 -0300

    Changed Sources folder name

--- Package.swift
+++ Package.swift
@@ -20,8 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TagMenu",
-            dependencies: [],
-            exclude: ["Example/**", "TagMenu.podspec"]),
+            dependencies: []),
         .testTarget(
             name: "TagMenuTests",
             dependencies: ["TagMenu"]),
