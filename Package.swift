b70ff88193253cd755a2ba2d271cacff6bb41a53
GabrielSilveiraa <gabi.msilveira@gmail.com>
2020-12-10 15:35:41 -0300

    Removed test target from Package

--- Package.swift
+++ Package.swift
@@ -22,9 +22,6 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "TagMenu",
-            dependencies: []),
-        .testTarget(
-            name: "TagMenuTests",
-            dependencies: ["TagMenu"]),
+            dependencies: [])
     ]
 )
